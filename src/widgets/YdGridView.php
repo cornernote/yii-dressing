1b1865f6618afa026da170002be5fe2231df7fc5
Brett O'Donnell <cornernote@gmail.com>
2013-09-23 14:33:43 +0930

    fix assetspath

--- src/widgets/YdGridView.php
+++ src/widgets/YdGridView.php
@@ -95,7 +95,7 @@ class YdGridView extends TbGridView
         parent::registerClientScript();
 
         if ($this->multiActions || $this->gridActions || $this->gridButtons) {
-            Yii::app()->clientScript->registerScriptFile(Yii::app()->dressing->assetPath . '/js/jquery.form.js');
+            Yii::app()->clientScript->registerScriptFile(Yii::app()->dressing->getAssetsUrl() . '/js/jquery.form.js');
             // put the url from the button into the form action
             // handle submit form to capture the response into a modal
             Yii::app()->controller->beginWidget('dressing.widgets.YdJavaScriptWidget', array('position' => CClientScript::POS_END));
