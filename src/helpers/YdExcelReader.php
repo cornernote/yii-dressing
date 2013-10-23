e242cc58a468fcf84d8dfd995f5959a5e76cdbf2
cornernote <cornernote@gmail.com>
2013-10-23 15:06:40 +1030

    added excel reader

---
+++ src/helpers/YdExcelReader.php
@@ -0,0 +1,20 @@
+<?php
+require_once(vp() . '/php-excel-reader/spreadsheet-reader/src/PHPExcelReader/SpreadsheetReader.php');
+
+/**
+ *
+ */
+class YdExcelReader
+{
+    /**
+     * @param $filename
+     * @return Spreadsheet_Excel_Reader
+     */
+    public static function read($filename)
+    {
+        $data = new PHPExcelReader\SpreadsheetReader();
+        $data->setOutputEncoding('CP1251');
+        $data->read($filename);
+        return $data->sheets;
+    }
+}
