789421d629499210adf6fa5380bbfa835597e2a2
Brett O'Donnell <cornernote@gmail.com>
2013-11-06 21:30:48 +1030

    Update YdConfig.php

--- src/components/YdConfig.php
+++ src/components/YdConfig.php
@@ -276,6 +276,36 @@ class YdConfig
                     'TbMenu' => array(
                         'activateParents' => true,
                     ),
+                    'TbCKEditor' => array(
+                        'editorOptions' => array(
+                            'toolbar_Full' => array(
+                                array('name' => 'document', 'items' => array('Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates')),
+                                array('name' => 'clipboard', 'items' => array('Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo')),
+                                array('name' => 'editing', 'items' => array('Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt')),
+                                array('name' => 'forms', 'items' => array('Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField')),
+                                array('name' => 'basicstyles', 'items' => array('Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat')),
+                                array('name' => 'paragraph', 'items' => array('NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl')),
+                                array('name' => 'links', 'items' => array('Link', 'Unlink', 'Anchor')),
+                                array('name' => 'insert', 'items' => array('Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe')),
+                                array('name' => 'styles', 'items' => array('Styles', 'Format', 'Font', 'FontSize')),
+                                array('name' => 'colors', 'items' => array('TextColor', 'BGColor')),
+                                array('name' => 'tools', 'items' => array('Maximize', 'ShowBlocks', '-', 'About')),
+                            ),
+                            'toolbar_Basic' => array(
+                                array('Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink', '-', 'About'),
+                            ),
+                            'toolbar_Dressing' => array(
+                                array('name' => 'tools', 'items' => array('Source', 'Maximize', 'ShowBlocks')),
+                                array('name' => 'clipboard', 'items' => array('Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo')),
+                                array('name' => 'basicstyles', 'items' => array('Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat')),
+                                array('name' => 'paragraph', 'items' => array('NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl')),
+                                array('name' => 'links', 'items' => array('Link', 'Unlink', 'Anchor')),
+                                array('name' => 'insert', 'items' => array('Image', 'Table', 'HorizontalRule', 'SpecialChar')),
+                                array('name' => 'styles', 'items' => array('Format')),
+                            ),
+                            'toolbar' => 'Custom',
+                        ),
+                    )
                 ),
             ),
             'dressing' => array(
