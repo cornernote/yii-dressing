ae21426f9eb1c216e220c41963b079991eafc74d
cornernote <cornernote@gmail.com>
2013-11-06 19:13:22 +1030

    show page load and mem even if audit is off

--- src/views/audit/_footer.php
+++ src/views/audit/_footer.php
@@ -9,14 +9,24 @@
  * @license http://www.gnu.org/copyleft/gpl.html
  */
 
-$audit = YdAudit::findCurrent();
-if (!$audit) return;
-
 $color = isset($color) ? $color : (YII_DEBUG ? 'inherit' : 'transparent');
-echo '<span class="small audit-id" style="color: ' . $color . ';">';
-echo '<!-- audit start -->audit-' . $audit->id . '<!-- audit end -->'; // html comment is used for extracting audit_id
-echo ' | ';
-echo number_format(microtime(true) - $audit->start_time, 2) . 'sec';
-echo ' | ';
-echo round(memory_get_peak_usage() / 1024 / 1024, 2) . 'mb';
-echo '</span>';
+
+$audit = YdAudit::findCurrent();
+if ($audit) {
+    echo '<span class="small audit-id" style="color: ' . $color . ';">';
+    echo '<!-- audit start -->audit-' . $audit->id . '<!-- audit end -->'; // html comment is used for extracting audit_id
+    echo ' | ';
+    echo number_format(microtime(true) - $audit->start_time, 2) . 'sec';
+    echo ' | ';
+    echo round(memory_get_peak_usage() / 1024 / 1024, 2) . 'mb';
+    echo '</span>';
+}
+else {
+    echo '<span class="small audit-id" style="color: ' . $color . ';">';
+    //echo '<!-- audit start -->audit-' . $audit->id . '<!-- audit end -->'; // html comment is used for extracting audit_id
+    //echo ' | ';
+    echo number_format(microtime(true) - APP_START, 2) . 'sec';
+    echo ' | ';
+    echo round(memory_get_peak_usage() / 1024 / 1024, 2) . 'mb';
+    echo '</span>';
+}
