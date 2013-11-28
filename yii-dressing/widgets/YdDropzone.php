9b4ddfd6fb5b5c6604df06e0e89d2686aa9a77aa
cornernote <cornernote@gmail.com>
2013-11-28 15:10:30 +1030

    added dropzone and simpletree widgets

---
+++ yii-dressing/widgets/YdDropzone.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ *
+ * Yii extension to the drag n drop HTML5 file upload Dropzone.js
+ * For more info, see @link http://www.dropzonejs.com/
+ *
+ * @link https://github.com/subdee/yii-dropzone
+ *
+ * @author Konstantinos Thermos
+ *
+ * @copyright
+ * Copyright (c) 2013 Konstantinos Thermos
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+ * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+class YdDropzone extends CWidget
+{
+    /**
+     * @var string The name of the file field
+     */
+    public $id = 'dropzone';
+    /**
+     * @var string The name of the file field
+     */
+    public $name = false;
+    /**
+     * @var CModel The model for the file field
+     */
+    public $model = false;
+    /**
+     * @var string The attribute of the model
+     */
+    public $attribute = false;
+    /**
+     * @var array An array of options that are supported by Dropzone
+     */
+    public $options = array();
+    /**
+     * @var string The URL that handles the file upload
+     */
+    public $url = false;
+    /**
+     * @var array An array of supported MIME types
+     */
+    public $mimeTypes = array();
+
+
+    /**
+     * @var string
+     */
+    public $onAddedFile = false;
+    /**
+     * @var string Called whenever a file is removed from the list. You can listen to this and delete the file from your server if you want to.
+     */
+    public $onRemovedFile = false;
+    /**
+     * @var string Receives an array of files and gets called whenever files are dropped or selected.
+     */
+    public $onSelectedFiles = false;
+    /**
+     * @var string When the thumbnail has been generated. Receives the dataUrl as second parameter.
+     */
+    public $onThumbnail = false;
+    /**
+     * @var string An error occured. Receives the errorMessage as second parameter and if the error was due to the XMLHttpRequest the xhr object as third.
+     */
+    public $onError = false;
+    /**
+     * @var string When a file gets processed (since there is a queue not all files are processed immediately).
+     */
+    public $onProcessing = false;
+    /**
+     * @var string Gets called periodically whenever the file upload progress changes.
+     * Gets the progress parameter as second parameter which is a percentage (0-100) and the bytesSent parameter as
+     * third which is the number of the bytes that have been sent to the server.  When an upload finishes dropzone
+     * ensures that uploadprogress will be called with a percentage of 100 at least once.
+     */
+    public $onUploadProgress = false;
+    /**
+     * @var string Called just before each file is sent. Gets the xhr object and the formData objects as second and
+     * third parameters, so you can modify them (for example to add a CSRF token) or add additional data.
+     */
+    public $onSending = false;
+    /**
+     * @var string The file has been uploaded successfully. Gets the server response as second argument.
+     */
+    public $onSuccess = false;
+    /**
+     * @var string Called when the upload was either successful or erroneous.
+     */
+    public $onComplete = false;
+    /**
+     * @var string Called when a file upload gets canceled.
+     */
+    public $onCanceled = false;
+    /**
+     * @var string Called when the number of files accepted exceeds the maxFiles limit.
+     */
+    public $onMaxFilesExceeded = false;
+
+    /**
+     * Create a div and the appropriate Javascript to make the div into the file upload area
+     */
+    public function run()
+    {
+        if (!$this->url)
+            $this->url = Yii::app()->getRequest()->getRequestUri();
+
+        echo CHtml::openTag('div', array('class' => 'dropzone', 'id' => $this->id));
+        echo CHtml::closeTag('div');
+
+        if (!$this->name && ($this->model && $this->attribute) && $this->model instanceof CModel)
+            $this->name = CHtml::activeName($this->model, $this->attribute);
+
+        $this->mimeTypes = CJavaScript::encode($this->mimeTypes);
+
+        $callbacks = array(
+            "this.on('addedfile',function(file){{$this->onAddedFile}});",
+            "this.on('removedfile',function(file){{$this->onRemovedFile}});",
+            "this.on('selectedfiles',function(files){{$this->onSelectedFiles}});",
+            "this.on('thumbnail',function(file, dataUrl){{$this->onThumbnail}});",
+            "this.on('error',function(file, errorMessage, xhr){{$this->onError}});",
+            "this.on('processing',function(file){{$this->onProcessing}});",
+            "this.on('uploadprogress',function(file, progress, bytesSent){{$this->onUploadProgress}});",
+            "this.on('sending',function(file, xhr, formData){{$this->onSending}});",
+            "this.on('success',function(file, response){{$this->onSuccess}});",
+            "this.on('complete',function(file){{$this->onComplete}});",
+            "this.on('canceled',function(file){{$this->onCanceled}});",
+            "this.on('maxfilesexceeded',function(file){{$this->onMaxFilesExceeded}});",
+        );
+
+        $options = CMap::mergeArray(array(
+            'url' => $this->url,
+            'parallelUploads' => 1,
+            'paramName' => $this->name,
+            //'accept' => "js:function(file, done){if(jQuery.inArray(file.type,{$this->mimeTypes}) == -1 ){done('File type not allowed.');}else{done();}}",
+            'init' => 'js:function(){' . implode(' ', $callbacks) . '}'
+        ), $this->options);
+
+        $options = CJavaScript::encode($options);
+
+        $script = "Dropzone.options.{$this->id} = {$options}";
+
+        $this->registerAssets();
+        Yii::app()->getClientScript()->registerScript(__CLASS__ . '#' . $this->getId(), $script, CClientScript::POS_END);
+    }
+
+    /**
+     *
+     */
+    private function registerAssets()
+    {
+        $baseUrl = Yii::app()->dressing->getAssetsUrl() . '/dropzone';
+        Yii::app()->getClientScript()->registerCoreScript('jquery');
+        Yii::app()->getClientScript()->registerScriptFile("{$baseUrl}/js/dropzone.js", CClientScript::POS_END);
+        Yii::app()->getClientScript()->registerCssFile("{$baseUrl}/css/dropzone.css");
+    }
+
+}
---
+++ yii-dressing/widgets/YdSimpleTreeWidget.php
@@ -0,0 +1,361 @@
+<?php
+
+class YdSimpleTreeWidget extends CInputWidget
+{
+    private $baseUrl;
+
+    public $ajaxUrl;
+    public $id = 'simpletree_widget';
+    public $model;
+    public $modelPropertyName = 'title';
+    public $modelPropertyId = 'id';
+    public $modelPropertyParentId = 'id_parent';
+    public $modelPropertyPosition = 'position';
+    public $modelPropertyUrl;
+    public $theme = 'default';
+    public $onSelect;
+    public $onCreate;
+    public $onMove;
+    public $onRemove;
+    public $onRename;
+
+    public function run()
+    {
+        //if this is an Ajax request, do Ajax and die.
+        //It is recommended to change $ajaxUrl to call SimpleTreeWidget::performAjax() directly from a controller.
+        if (Yii::app()->request->isAjaxRequest && isset($_POST['simpletree'])) {
+            self::performAjax();
+            die;
+        }
+        //register assets
+        $this->baseUrl = Yii::app()->dressing->getAssetsUrl() . '/jstree';
+        $clientScript = Yii::app()->getClientScript();
+        $clientScript->registerCoreScript('jquery');
+        $clientScript->registerScriptFile($this->baseUrl . '/_lib/jquery.cookie.js', CClientScript::POS_HEAD);
+        $clientScript->registerScriptFile($this->baseUrl . '/_lib/jquery.hotkeys.js', CClientScript::POS_HEAD);
+        $clientScript->registerScriptFile($this->baseUrl . '/jquery.jstree.js', CClientScript::POS_HEAD);
+
+
+        //create container node
+        echo "<div id='$this->id'>node</div>";
+        $clientScript->registerScript('createJtree', 'jQuery("#' . $this->id . '").jstree({
+            "plugins" : [ "themes", "json_data", "ui", "crrm", "cookies", "dnd", "types", "hotkeys", "contextmenu" ],
+            "core" : {
+                "animation" : 300
+            },
+            "json_data" : {
+                "ajax" : {
+                    // the URL to fetch the data
+                    "url" : "' . $this->ajaxUrl . '",
+                    "data" : function (n) { 
+                        return { 
+                            "operation" : "get_children", 
+                            "id" : n.attr ? n.attr("id").replace("node_","") : ' . $this->modelId . ' ,
+                            ' . $this->getEnvironment() . '
+                        }; 
+                    }
+                }
+                
+            },
+            "themes" : {
+                "theme" : "' . $this->theme . '",
+                "url" : "' . $this->baseUrl . '/themes/' . $this->theme . '/style.css"
+            },
+            "types" : {
+                "valid_children" : [],
+                "max_depth" : -2,
+                "max_children" : -2,
+                "types" : {
+                    "readonly" : {
+                        "delete_node" : false,
+                        "icon" : {"image" : "' . $this->baseUrl . '/themes/readonly.png"},
+                    }
+                }
+            }
+        })
+        .bind("create.jstree", function (e, data) {
+            $.post(
+                "' . $this->ajaxUrl . '",
+                { 
+                    "simpletree" : 1,
+                    "operation" : "create_node", 
+                    "id" : data.rslt.parent.attr("id").replace("node_",""), 
+                    "position" : data.rslt.position,
+                    "title" : data.rslt.name,
+                    "type" : data.rslt.obj.attr("rel"),
+                    ' . $this->getEnvironment() . '
+                }, 
+                function (r) {
+                    if(r.status) {
+                        $(data.rslt.obj).attr("id", "node_" + r.id);
+                        ' . $this->onCreate . '
+                    }
+                    else {
+                        $.jstree.rollback(data.rlbk);
+                    }
+                }
+            );
+        })
+        .bind("remove.jstree", function (e, data) {
+            data.rslt.obj.each(function () {
+                if (data.inst._get_type(this) == "readonly")
+                    return; 
+                $.ajax({
+                    async : false,
+                    type: \'POST\',
+                    url: "' . $this->ajaxUrl . '",
+                    data : { 
+                        "simpletree" : 1,
+                        "operation" : "remove_node", 
+                        "id" : this.id.replace("node_",""),
+                        ' . $this->getEnvironment() . '
+                    }, 
+                    success : function (r) {
+                        if(!r.status) {
+                            data.inst.refresh();
+                        }else{
+                        
+                            ' . $this->onRemove . '
+                        }
+                    }
+                });
+            });
+        })
+        .bind("rename.jstree", function (e, data) {
+            $.post(
+                "' . $this->ajaxUrl . '",
+                { 
+                    "simpletree" : 1,
+                    "operation" : "rename_node", 
+                    "id" : data.rslt.obj.attr("id").replace("node_",""),
+                    "title" : data.rslt.new_name,
+                    ' . $this->getEnvironment() . '
+                }, 
+                function (r) {
+                    if(!r.status) {
+                        $.jstree.rollback(data.rlbk);
+                    }else{
+                        ' . $this->onRename . '
+                    }
+                }
+            );
+        })
+        .bind("move_node.jstree", function (e, data) {
+            data.rslt.o.each(function (i) {
+                $.ajax({
+                    async : false,
+                    type: \'POST\',
+                    url: "' . $this->ajaxUrl . '",
+                    data : { 
+                        "simpletree" : 1,
+                        "operation" : "move_node", 
+                        "id" : $(this).attr("id").replace("node_",""), 
+                        "ref" : data.rslt.np.attr("id").replace("node_",""), 
+                        "position" : data.rslt.cp + i,
+                        "title" : data.rslt.name,
+                        "copy" : data.rslt.cy ? 1 : 0,
+                        ' . $this->getEnvironment() . '
+                    },
+                    success : function (r) {
+                        if(!r.status) {
+                            $.jstree.rollback(data.rlbk);
+                        }
+                        else {
+                            $(data.rslt.oc).attr("id", "node_" + r.id);
+                            if(data.rslt.cy) {
+                                data.inst.refresh(data.inst._get_parent(data.rslt.oc));
+                            }
+                            ' . $this->onMove . '
+                        }
+                    }
+                });
+            });
+        })
+        .bind("select_node.jstree", function (e, data){
+            ' . $this->onSelect . '
+        });', CClientScript::POS_END);
+    }
+
+    public function getEnvironment()
+    {
+        $model = is_string($this->model) ? $this->model : get_class($this->model);
+        return '
+            "model" : "' . $model . '",
+            "modelPropertyId" : "' . $this->modelPropertyId . '",
+            "modelPropertyParentId" : "' . $this->modelPropertyParentId . '",
+            "modelPropertyPosition" : "' . $this->modelPropertyPosition . '",
+            "modelPropertyName" : "' . $this->modelPropertyName . '",
+            "modelPropertyUrl" : "' . $this->modelPropertyUrl . '",
+        ';
+    }
+
+
+    public function getModelId()
+    {
+        if (is_object($this->model))
+            return ($this->model->{$this->modelPropertyId});
+        else
+            return 0;
+    }
+
+    static function _get_children()
+    {
+
+        $children = array();
+        $Model = new $_REQUEST['model'];
+        foreach ($Model->findAllByAttributes(array($_REQUEST['modelPropertyParentId'] => $_REQUEST['id'])) AS $k => $Model) {
+            if (strpos($_REQUEST['modelPropertyName'], '.') !== false) {
+                $modelPropertyName = explode('.', $_REQUEST['modelPropertyName']);
+                $children[$k]["data"] = $Model->{$modelPropertyName[0]}->{$modelPropertyName[1]};
+            }
+            else
+                $children[$k]["data"] = $Model->{$_REQUEST['modelPropertyName']};
+            $children[$k]["attr"]["id"] = "node_" . $Model->{$_REQUEST['modelPropertyId']};
+            if ($_REQUEST['modelPropertyUrl'])
+                $children[$k]["attr"]["href"] = $Model->{$_REQUEST['modelPropertyUrl']};
+            if (isset($Model->readonly) && $Model->readonly)
+                $children[$k]["attr"]["rel"] = "readonly";
+            else
+                $children[$k]["attr"]["rel"] = "default";
+
+            //only add a closed symbol if the model has children
+            if ($Model->findByAttributes(array($_REQUEST['modelPropertyParentId'] => $Model->{$_REQUEST['modelPropertyId']})))
+                $children[$k]["state"] = "closed";
+        };
+
+        if (!$_REQUEST['id'])
+            $children = array(
+                "data" => "root",
+                "attr" => array('id' => 0),
+                "children" => $children,
+                "state" => $children ? 'open' : '',
+                "attr" => array("rel" => "readonly")
+            );
+
+        echo json_encode($children);
+        die;
+    }
+
+
+    static function performAjax()
+    {
+        $Model = new $_REQUEST['model'];
+        $method = '_' . $_REQUEST['operation'];
+
+        header("HTTP/1.0 200 OK");
+        header('Content-type: text/json; charset=utf-8');
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: no-cache");
+        self::$method($_POST);
+
+    }
+
+    static function _create_node($params)
+    {
+        $Model = new $params['model'];
+        $Model->$params['modelPropertyParentId'] = $params['id'];
+        $Model->$params['modelPropertyName'] = $params['title'];
+        @$Model->$params['modelPropertyPosition'] = $params['position'];
+
+        if ($Model->save())
+            echo json_encode(array('status' => 1, 'id' => $Model->$params['modelPropertyId']));
+        else
+            print_r($Model->getErrors());
+    }
+
+    static function _remove_node($params)
+    {
+        self::_removeModelRecursively($params);
+        echo json_encode(array('status' => 1));
+    }
+
+    static function _rename_node($params)
+    {
+        $Model = new $params['model'];
+        $Model = $Model->findByPk($params['id']);
+        $Model->$params['modelPropertyName'] = $params['title'];
+        if ($Model->save())
+            echo json_encode(array('status' => 1));
+    }
+
+
+    static function _move_node($params)
+    {
+        $params['ref'] = (int)$params['ref'];
+
+        //get model
+        $Model = new $params['model'];
+        $Model = $Model->findByPk($params['id']);
+
+        //copy and die?
+        if ($params['copy']) {
+            $Model = self::_copy_node($Model, $params);
+        }
+
+        //get new siblings
+        $criteria = new CDbCriteria;
+        $criteria->order = $params['modelPropertyPosition'];
+        $criteria->condition = $params['modelPropertyParentId'] . '=' . $params['ref'] . ' AND ' . $params['modelPropertyId'] . '!=' . $params['id'];
+        $siblings = $Model->findAll($criteria);
+
+        //if item is moved to a higher position ID in it's current folder, make sure to substract it's old position as the item only exists once
+        if ($Model->$params['modelPropertyParentId'] == $params['ref'] && $Model->$params['modelPropertyPosition'] < $params['position'])
+            $params['position']--;
+
+        //save model
+        $Model->$params['modelPropertyPosition'] = $params['position'];
+        $Model->$params['modelPropertyParentId'] = $params['ref'];
+        $Model->save();
+
+        //assign positions to siblings
+        $i = 0;
+        foreach ($siblings AS $Sibling) {
+            //params position is reserved, so iterate by it
+            if ($i == $params['position'])
+                $i++;
+            $Sibling->$params['modelPropertyPosition'] = $i;
+            $Sibling->save();
+            $i++;
+        }
+
+        echo json_encode(array('status' => 1));
+    }
+
+    static function _copy_node($Model, $params, $inheritPosition = false)
+    {
+        $NewModel = new $params['model'];
+        $NewModel->attributes = $Model->attributes;
+
+        //copy these, even if they're unsafe values
+        $NewModel->{$params['modelPropertyName']} = $Model->{$params['modelPropertyName']};
+        $NewModel->{$params['modelPropertyParentId']} = $params['ref'];
+        $NewModel->{$params['modelPropertyPosition']} = $inheritPosition ? $Model->{$params['modelPropertyPosition']} : $params['position'];
+
+        if ($NewModel->save()) {
+            //copy children
+            foreach ($NewModel->findAllByAttributes(array($params['modelPropertyParentId'] => $Model->{$params['modelPropertyId']})) AS $Child) {
+                $params['ref'] = $NewModel->{$params['modelPropertyId']};
+                self::_copy_node($Child, $params, true);
+            }
+            return $NewModel;
+        }
+    }
+
+    static function _removeModelRecursively($params)
+    {
+        if (!$params['id'] || preg_match('/[^\d]/', $params['id']))
+            return json_encode(array('status' => 0));
+
+        $Model = new $params['model'];
+        $Model = $Model->findByPk($params['id']);
+        foreach ($Model->findAllByAttributes(array($params['modelPropertyParentId'] => $params['id'])) AS $Child) {
+            $params['id'] = $Child->$params['modelPropertyId'];
+            self::_removeModelRecursively($params);
+        }
+        $Model->delete();
+    }
+}
+
+
+?>
