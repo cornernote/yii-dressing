c79d3b21b1a4e99a1e16e2e4488f3ee7bb54e90b
cornernote <cornernote@gmail.com>
2013-12-03 17:14:51 +1030

    remove unused defines

--- yii-dressing/defines.php
+++ yii-dressing/defines.php
@@ -24,16 +24,6 @@ defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 defined('YII_DRESSING_CLI') or define('YII_DRESSING_CLI', (substr(php_sapi_name(), 0, 3) == 'cli'));
 
 /**
- * Defines the Yii dressing path.
- */
-//defined('YII_DRESSING_PATH') or define('YII_DRESSING_PATH', str_replace(array('\\', '/'), DS, realpath(dirname(__FILE__))));
-
-/**
- * Defines Yii dressing log levels, comma separated list of: trace, info, error, warning, profile
- */
-//defined('YII_DRESSING_LOG_LEVELS') or define('YII_DRESSING_LOG_LEVELS', 'error, warning');
-
-/**
  * Defines the Vendor path.
  */
 defined('VENDOR_PATH') or define('VENDOR_PATH', str_replace(array('\\', '/'), DS, dirname(dirname(dirname(dirname(__FILE__))))));
@@ -52,11 +42,6 @@ defined('YII_DRESSING_HASH') or define('YII_DRESSING_HASH', false);
  * Defines if we should use the yii-debug-toolbar.
  */
 defined('YII_DEBUG_TOOLBAR') or define('YII_DEBUG_TOOLBAR', false);
-
-/**
- * Defines the filesystem path to the application.
- */
-//defined('APP_PATH') or define('APP_PATH', str_replace(array('\\', '/'), DS, dirname(VENDOR_PATH) . DS . 'app'));
 
 /**
  * Defines the filesystem path to the public directory of the application.
