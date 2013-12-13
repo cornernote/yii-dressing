992360685e08ed830a363e696a5c0a616b1a89c1
Brett O'Donnell <cornernote@gmail.com>
2013-12-13 23:22:56 +1030

    Update YdBase.php

--- yii-dressing/YdBase.php
+++ yii-dressing/YdBase.php
@@ -4,7 +4,7 @@
  *
  * @author Brett O'Donnell <cornernote@gmail.com>
  * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
+ * @link https://github.com/cornernote/yii-dressing
  * @license BSD-3-Clause https://raw.github.com/cornernote/yii-dressing/master/license.txt
  */
 
