8657440d516a4fdf136de1a2c0e76f7211081377
cornernote <cornernote@gmail.com>
2013-12-16 17:10:57 +1030

    added dynamic menu

---
+++ yii-dressing/components/YdDynamicMenu.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * YdDynamicMenu
+ */
+class YdDynamicMenu
+{
+    /**
+     * @static
+     * @param $item
+     * @param string $menu
+     */
+    public static function add($item, $menu = 'menu')
+    {
+        $data = user()->getState($menu, array());
+        $item['active'] = false;
+        array_unshift($data, $item);
+        while (count($data) > 20) array_pop($data);
+        $labels = array();
+        foreach ($data as $k => $v) {
+            if (in_array($v['label'], $labels)) {
+                unset($data[$k]);
+                continue;
+            }
+            $labels[] = $v['label'];
+        }
+        user()->setState($menu, $data);
+    }
+
+    /**
+     * @static
+     * @param string $menu
+     * @return array
+     */
+    public static function output($menu = 'menu')
+    {
+        $data = user()->getState($menu, array());
+        if (empty($data)) return $data;
+        return array(
+            'class' => 'bootstrap.widgets.TbMenu',
+            'items' => array(
+                array(
+                    'label' => Yii::t('dressing', 'History'),
+                    'items' => $data,
+                ),
+            ),
+        );
+    }
+
+}
