60c8b4099d9f6b39be6024d558d265a5cc5ac76a
cornernote <cornernote@gmail.com>
2013-12-16 21:11:52 +1030

    fix missing Yd

--- yii-dressing/views/tool/index.php
+++ yii-dressing/views/tool/index.php
@@ -8,29 +8,29 @@ $this->pageTitle = Yii::t('dressing', 'Tools');
 echo '<h2>' . t('Manage') . '</h2>';
 $this->widget('bootstrap.widgets.TbMenu', array(
     'type' => 'pills',
-    'items' => SiteMenu::getItemsFromMenu('Manage', SiteMenu::MENU_ADMIN),
+    'items' => YdSiteMenu::getItemsFromMenu('Manage', YdSiteMenu::MENU_ADMIN),
 ));
 
 echo '<h2>' . t('Settings') . '</h2>';
 $this->widget('bootstrap.widgets.TbMenu', array(
     'type' => 'pills',
-    'items' => SiteMenu::getItemsFromMenu('Settings', SiteMenu::MENU_ADMIN),
+    'items' => YdSiteMenu::getItemsFromMenu('Settings', YdSiteMenu::MENU_ADMIN),
 ));
 
 echo '<h2>' . t('Reports') . '</h2>';
 $this->widget('bootstrap.widgets.TbMenu', array(
     'type' => 'pills',
-    'items' => SiteMenu::getItemsFromMenu('Reports', SiteMenu::MENU_ADMIN),
+    'items' => YdSiteMenu::getItemsFromMenu('Reports', YdSiteMenu::MENU_ADMIN),
 ));
 
 echo '<h2>' . t('Logs') . '</h2>';
 $this->widget('bootstrap.widgets.TbMenu', array(
     'type' => 'pills',
-    'items' => SiteMenu::getItemsFromMenu('Logs', SiteMenu::MENU_ADMIN),
+    'items' => YdSiteMenu::getItemsFromMenu('Logs', YdSiteMenu::MENU_ADMIN),
 ));
 
 echo '<h2>' . t('Tools') . '</h2>';
 $this->widget('bootstrap.widgets.TbMenu', array(
     'type' => 'pills',
-    'items' => SiteMenu::getItemsFromMenu('Tools', SiteMenu::MENU_ADMIN),
+    'items' => YdSiteMenu::getItemsFromMenu('Tools', YdSiteMenu::MENU_ADMIN),
 ));
