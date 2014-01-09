caeba46fc72f4e1374c5786b243da74e93e5ef01
cornernote <cornernote@gmail.com>
2014-01-09 12:02:16 +1030

    update ckeditor

--- yii-dressing/widgets/YdCKEditor.php
+++ yii-dressing/widgets/YdCKEditor.php
@@ -3,70 +3,70 @@
 /**
  *
  */
-class YdCKEditor extends CInputWidget
+class YdCKEditor extends CWidget
 {
 
     /**
-     * @var
+     * @var string
      */
-    public $defaultValue;
-    /**
-     * @var
-     */
-    public $config;
+    public $selector = 'textarea.ckeditor';
 
     /**
      * @var
      */
-    protected $url;
+    public $config = array();
 
     /**
-     * @var
+     * @var array
      */
-    protected $path;
-
+    public $defaultConfig = array(
+        //'toolbar_Full' => array(
+        //    array('name' => 'document', 'items' => array('Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates')),
+        //    array('name' => 'clipboard', 'items' => array('Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo')),
+        //    array('name' => 'editing', 'items' => array('Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt')),
+        //    array('name' => 'forms', 'items' => array('Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField')),
+        //    array('name' => 'basicstyles', 'items' => array('Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat')),
+        //    array('name' => 'paragraph', 'items' => array('NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl')),
+        //    array('name' => 'links', 'items' => array('Link', 'Unlink', 'Anchor')),
+        //    array('name' => 'insert', 'items' => array('Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe')),
+        //    array('name' => 'styles', 'items' => array('Styles', 'Format', 'Font', 'FontSize')),
+        //    array('name' => 'colors', 'items' => array('TextColor', 'BGColor')),
+        //    array('name' => 'tools', 'items' => array('Maximize', 'ShowBlocks', '-', 'About')),
+        //),
+        //'toolbar_Basic' => array(
+        //    array('Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink', '-', 'About'),
+        //),
+        'toolbar_Full' => array(
+            array('name' => 'tools', 'items' => array('Source', 'Maximize', 'ShowBlocks')),
+            array('name' => 'clipboard', 'items' => array('Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo')),
+            array('name' => 'basicstyles', 'items' => array('Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat')),
+            array('name' => 'paragraph', 'items' => array('NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock')),
+            array('name' => 'links', 'items' => array('Link', 'Unlink', 'Anchor')),
+            array('name' => 'insert', 'items' => array('Image', 'Table', 'HorizontalRule', 'SpecialChar')),
+            array('name' => 'styles', 'items' => array('Format')),
+        ),
+        'toolbar_Basic' => array(
+            array('Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink'),
+        ),
+        'toolbar' => 'Full',
+        'height' => '400px',
+        'width' => '100%',
+    );
 
     /**
      *
      */
     public function init()
     {
-        $this->path = Yii::getPathOfAlias('vendor.cornernote.ckeditor-assets');
-        $this->url = app()->assetManager->publish($this->path, false, -1, YII_DEBUG);
-        if (substr($this->id, 0, 2) == 'yw' && $this->hasModel()) {
-            $this->id = get_class($this->model) . '[' . $this->attribute . ']';
-        }
+        $baseUrl = app()->assetManager->publish(Yii::getPathOfAlias('vendor.cornernote.ckeditor-assets.ckeditor'), false, -1, YII_DEBUG);
+        $clientScript = Yii::app()->clientScript;
+        $clientScript->registerScriptFile($baseUrl . '/ckeditor.js');
+        $clientScript->registerScriptFile($baseUrl . '/adapters/jquery.js');
 
-        if (!isset($this->config['toolbar'])) {
-            $this->config['toolbar'] = array(
-                array('Source', 'RemoveFormat'),
-                array('Bold', 'Italic', 'Underline', 'Strike', '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'HorizontalRule'),
-                array('Image', 'Link', 'Unlink', 'Anchor'),
-                array('Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt'),
-                array('Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'),
-            );
-        }
-        if (!isset($this->config['height'])) {
-            $this->config['height'] = '400px';
-        }
-        if (!isset($this->config['width'])) {
-            $this->config['width'] = '100%';
-        }
+        $this->config = CMap::mergeArray($this->config, $this->defaultConfig);
+        $clientScript->registerScript($this->id, '$("' . $this->selector . '").ckeditor(' . json_encode($this->config) . ');', CClientScript::POS_READY);
+
         parent::init();
-    }
-
-    /**
-     *
-     */
-    public function run()
-    {
-        require_once($this->path . '/ckeditor.php');
-        $oCKeditor = new CKeditor($this->id);
-        $oCKeditor->basePath = $this->url . '/';
-        foreach ($this->config as $key => $value) {
-            $oCKeditor->config[$key] = $value;
-        }
-        $oCKeditor->editor($this->id, $this->defaultValue);
     }
 
 }
