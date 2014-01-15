908d4f318fb7724d86f1b0e9046a83fc661757fe
cornernote <cornernote@gmail.com>
2014-01-15 17:23:38 +1030

    cleanup

--- yii-dressing/widgets/YdPreventDoubleSubmit.php
+++ yii-dressing/widgets/YdPreventDoubleSubmit.php
@@ -11,15 +11,9 @@ class YdPreventDoubleSubmit extends CWidget
      */
     public function init()
     {
-        $script = "// prevent double submit
-        $(document).on('submit', 'form:not(.js-allow-double-submit)', function (e) {
-            var form = $(this);
-            // Previously submitted - don't submit again
-            if (form.data('submitted') === true) { e.preventDefault(); }
-            // Mark it so that the next submit can be ignored
-            else { form.data('submitted', true); }
-        });";
-        Yii::app()->clientScript->registerScript($this->id, $script, CClientScript::POS_READY);
+        Yii::app()->clientScript->registerScript($this->id, "$(document).on('submit', 'form:not(.allow-double-submit)', function (e) {
+            var form = $(this); if (form.data('submitted') !== true) { form.data('submitted', true); } else {  e.preventDefault(); }
+        });", CClientScript::POS_END);
         parent::init();
     }
 
