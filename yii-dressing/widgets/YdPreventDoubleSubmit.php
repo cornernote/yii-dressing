cbc1a872e6304cfcd2d301836ec30f320e015020
cornernote <cornernote@gmail.com>
2014-01-15 17:20:43 +1030

    cleanup

--- yii-dressing/widgets/YdPreventDoubleSubmit.php
+++ yii-dressing/widgets/YdPreventDoubleSubmit.php
@@ -11,9 +11,6 @@ class YdPreventDoubleSubmit extends CWidget
      */
     public function init()
     {
-        $baseUrl = Yii::app()->dressing->getAssetsUrl();
-        $clientScript = Yii::app()->clientScript;
-        $clientScript->registerScriptFile($baseUrl . '/js/prevent-double-submit.js');
         $script = "// prevent double submit
         $(document).on('submit', 'form:not(.js-allow-double-submit)', function (e) {
             var form = $(this);
@@ -22,7 +19,7 @@ class YdPreventDoubleSubmit extends CWidget
             // Mark it so that the next submit can be ignored
             else { form.data('submitted', true); }
         });";
-        $clientScript->registerScript($this->id, $script, CClientScript::POS_READY);
+        Yii::app()->clientScript->registerScript($this->id, $script, CClientScript::POS_READY);
         parent::init();
     }
 
