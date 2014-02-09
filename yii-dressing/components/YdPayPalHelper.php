9bec0f024bb04fb762dac44ca170790c3e88553c
cornernote <cornernote@gmail.com>
2014-02-09 23:33:23 +1030

    update paypal helper

--- yii-dressing/components/YdPayPalHelper.php
+++ yii-dressing/components/YdPayPalHelper.php
@@ -1,5 +1,7 @@
 <?php
+
 /**
+ * PayPal Helper
  *
  * Sandbox Accounts:
  * Seller user/pass = seller_1350383460_biz@mrphp.com.au / 350383305
@@ -13,6 +15,8 @@
  * 'identityToken' => 'ifcqMvmR1e7j2E9ZfLwSG_p5eDRxg3PuQUe7Ri-Zo5SRrxKksr7qWKUfb-m',
  * 'business' => 'seller_1350383460_biz@mrphp.com.au',
  *
+ * @var array $defaultOptions
+ * @var string $imageAltText
  *
  * @author Brett O'Donnell <cornernote@gmail.com>
  * @author Zain Ul abidin <zainengineer@gmail.com>
@@ -26,82 +30,149 @@ class YdPayPalHelper extends CApplicationComponent
 {
 
     /**
+     * @var string PayPal account email address
+     */
+    public $business;
+
+    /**
      * @var string sandbox or live
      */
     public $environment = 'sandbox';
 
     /**
+     * @link https://developer.paypal.com/docs/classic/paypal-payments-standard/integration-guide/Appx_websitestandard_htmlvariables/
+     * @link https://cms.paypal.com/us/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_html_IPNandPDTVariables
      * @var array PayPal Options
-     * @ref https://cms.paypal.com/us/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_html_IPNandPDTVariables
      */
-    public $options = array();
+    public $defaultOptions = array(
+        //// example options
+        //'cmd' => '_xclick-subscriptions',
+        //'return' => 'https://localhost/checkout/success',
+        //'cancel_return' => 'https://localhost/checkout/plan',
+        //'notify_url' => 'https://localhost/checkout/ipn',
+        //'currency_code' => 'AUD',
+        //'p3' => 1,
+        //'t3' => 'M',
+        //'no_shipping' => 1,
+        //'no_note' => 1,
+        //'charset' => 'utf-8',
+        //// overwrite these using $options in linkButton() and formButton()
+        //'item_name' => 'Subscription',
+        //'item_number' => 'subscription',
+        //'a3' => 1.00,
+        //'custom' => '',
+        //'invoice' => '',
+    );
 
     /**
-     *
+     * @link https://developer.paypal.com/webapps/developer/docs/classic/archive/buttons/
+     * @var string
      */
-    public function init()
-    {
-        $this->options = CMap::mergeArray(array(
-            'cmd' => '_xclick-subscriptions',
-            'business' => 'webmaster@localhost',
-            'lc' => 'AU',
-            'no_note' => 1,
-            'no_shipping' => 1,
-            'rm' => 1,
-            'return' => 'https://localhost/checkout/success',
-            'cancel_return' => 'https://localhost/checkout/plan',
-            'notify_url' => 'https://localhost/checkout/ipn',
-            'src' => 1,
-            'p3' => 1,
-            't3' => 'M',
-            'currency_code' => 'AUD',
-            'bn' => 'PP-SubscriptionsBF:btn_subscribeCC_LG.gif:NonHosted',
-            // overwrite these using $options
-            'item_name' => 'Subscription',
-            'item_number' => 'subscription',
-            'a3' => 1.00,
-            'custom' => '',
-            'invoice' => '',
-        ), $this->options);
-    }
+    public $image = 'https://www.paypalobjects.com/en_AU/i/btn/btn_subscribe_LG.gif';
 
     /**
-     * @param $options
-     * @return string
+     * @var string
      */
-    public function getButton($options)
-    {
-        $options = $this->getOptions($options);
-        $button = '<form action="https://www.' . ($this->environment != 'live' ? 'sandbox.' : '') . 'paypal.com/cgi-bin/webscr" method="get">';
-        foreach ($options as $k => $v) {
-            $button .= '<input type="hidden" name="' . $k . '" value="' . $v . '">';
-        }
-        $button .= '<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">';
-        $button .= '<img alt="" border="0" src="https://www.paypalobjects.com/en_AU/i/scr/pixel.gif" width="1" height="1">';
-        $button .= '</form>';
-        return $button;
-    }
-
-    /**
-     * @param $options
-     * @return string
-     */
-    public function getLink($options)
-    {
-        $options = $this->getOptions($options);
-        $link = array();
-        foreach ($options as $k => $v) {
-            $link[] = urlencode($k) . '=' . urlencode($v);
-        }
-        return 'https://www.' . ($this->environment != 'live' ? 'sandbox.' : '') . 'paypal.com/cgi-bin/webscr?' . implode('&', $link);
-    }
+    private $imageAltText = 'PayPal - The safer, easier way to pay online.';
 
     /**
      * @return string
      */
-    public function getUnsubscribeLink()
+    public function url()
     {
-        return 'https://www.' . ($this->environment != 'live' ? 'sandbox.' : '') . 'paypal.com/cgi-bin/webscr?cmd=_manage-paylist';
+        return 'https://www.' . ($this->environment != 'live' ? 'sandbox.' : '') . 'paypal.com/cgi-bin/webscr';
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    public function formButton($options = array())
+    {
+        $form = CHtml::beginForm($this->url(), 'get');
+        CHtml::hiddenField('business', $this->business);
+        foreach (CMap::mergeArray($this->defaultOptions, $options) as $k => $v)
+            $form .= CHtml::hiddenField($k, $v);
+        $form .= CHtml::imageButton($this->image, array('border' => 0, 'alt' => $this->imageAltText));
+        $form .= CHtml::endForm();
+        return $form;
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    public function linkButton($options = array())
+    {
+        $options = CMap::mergeArray($this->defaultOptions, $options);
+        $options['business'] = $this->business;
+        $image = CHtml::image($this->image, $this->imageAltText);
+        $url = $this->url() . ($options ? '?' . http_build_query($options) : '');
+        return CHtml::link($image, $url);
+    }
+
+    /**
+     * @return string
+     */
+    public function unsubscribeLink()
+    {
+        return $this->url() . '?cmd=_manage-paylist';
+    }
+
+    /**
+     * @link https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNIntro/
+     * @param $data - $_POST
+     * @param $agent - $_SERVER['HTTP_USER_AGENT']
+     * @param $amount
+     * @param $tax
+     * @param $currency
+     * @param $invoice
+     * @param $custom
+     * @param $itemNumber
+     * @param $quantity
+     * @return array of errors, or empty array if no errors
+     */
+    public function verifyIpn($data, $agent, $amount, $tax, $currency, $invoice, $custom, $itemNumber, $quantity)
+    {
+        $errors = array();
+
+        if (trim(file_get_contents($this->url() . '?cmd=_notify-validate&' . http_build_query($data))) != 'VERIFIED')
+            $errors[] = 'Invalid IPN request';
+
+        if (strpos($agent, 'PayPal IPN') === false)
+            $errors[] = 'Invalid user agent';
+
+        if ($data['payment_status'] != 'Complete')
+            $errors[] = 'Invalid payment status';
+
+        if (abs($data['mc_gross'] - $amount) < 0.009)
+            $errors[] = 'Invalid amount';
+
+        if (abs($data['tax'] - $tax) < 0.009)
+            $errors[] = 'Invalid tax';
+
+        if ($data['mc_currency'] != $currency)
+            $errors[] = 'Invalid currency';
+
+        if ($data['business'] != $this->business)
+            $errors[] = 'Invalid business';
+
+        if ($data['receiver_email'] != $this->business)
+            $errors[] = 'Invalid receiver email';
+
+        if ($data['quantity'] != $quantity)
+            $errors[] = 'Invalid quantity';
+
+        if ($data['invoice'] != $invoice)
+            $errors[] = 'Invalid invoice';
+
+        if ($data['custom'] != $custom)
+            $errors[] = 'Invalid custom';
+
+        if ($data['item_number'] != $itemNumber)
+            $errors[] = 'Invalid item number';
+
+        return $errors;
     }
 
 }
