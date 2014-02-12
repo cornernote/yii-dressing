f42568b068521670a0240783f0caed768534ca77
cornernote <cornernote@gmail.com>
2014-02-13 02:50:55 +1030

    added function for pretty json

--- yii-dressing/components/YdStringHelper.php
+++ yii-dressing/components/YdStringHelper.php
@@ -183,4 +183,68 @@ class YdStringHelper
         return $destroy ? '' : $matches[0];
     }
 
+    /**
+     * @param string $json
+     * @return string
+     */
+    static public function prettyJson($json)
+    {
+        $result = '';
+        $level = 0;
+        $prev_char = '';
+        $in_quotes = false;
+        $ends_line_level = NULL;
+        $json_length = strlen($json);
+
+        for ($i = 0; $i < $json_length; $i++) {
+            $char = $json[$i];
+            $new_line_level = NULL;
+            $post = "";
+            if ($ends_line_level !== NULL) {
+                $new_line_level = $ends_line_level;
+                $ends_line_level = NULL;
+            }
+            if ($char === '"' && $prev_char != '\\') {
+                $in_quotes = !$in_quotes;
+            }
+            else if (!$in_quotes) {
+                switch ($char) {
+                    case '}':
+                    case ']':
+                        $level--;
+                        $ends_line_level = NULL;
+                        $new_line_level = $level;
+                        break;
+
+                    case '{':
+                    case '[':
+                        $level++;
+                    case ',':
+                        $ends_line_level = $level;
+                        break;
+
+                    case ':':
+                        $post = " ";
+                        break;
+
+                    case " ":
+                    case "\t":
+                    case "\n":
+                    case "\r":
+                        $char = "";
+                        $ends_line_level = $new_line_level;
+                        $new_line_level = NULL;
+                        break;
+                }
+            }
+            if ($new_line_level !== NULL) {
+                $result .= "\n" . str_repeat("\t", $new_line_level);
+            }
+            $result .= $char . $post;
+            $prev_char = $char;
+        }
+
+        return $result;
+    }
+
 }
