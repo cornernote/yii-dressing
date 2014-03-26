a4fb2f4cd73d1a7c4dc3669a9dc9ab208112b82b
cornernote <cornernote@gmail.com>
2014-03-26 19:48:42 +1030

    added minfraud helper

---
+++ yii-dressing/components/YdMinFraud.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * YdMinFraud
+ *
+ * @link http://www.maxmind.com/app/ccv
+ *
+ * Usage:
+ * Yii::app()->minFraud->getMinFraud(array(
+ *     'license_key' => 'YOUR_LICENSE_KEY_HERE',
+ *     'i' => '24.24.24.24',
+ *     'domain' => 'yahoo.com',
+ *     'city' => 'New+York',
+ *     'region' => 'NY',
+ *     'postal' => '10011',
+ *     'country' => 'US',
+ *     'bin' => '549099'
+ * ));
+ *
+ * @author Brett O'Donnell <cornernote@gmail.com>
+ * @author Zain Ul abidin <zainengineer@gmail.com>
+ * @copyright 2013 Mr PHP
+ * @link https://github.com/cornernote/yii-dressing
+ * @license BSD-3-Clause https://raw.github.com/cornernote/yii-dressing/master/license.txt
+ *
+ * @package dressing.components
+ */
+class YdMinFraud extends CApplicationComponent
+{
+
+    /**
+     * @var string
+     */
+    public $licenseKey;
+
+    /**
+     * @param $options
+     * @return array
+     */
+    public function getMinFraud($options)
+    {
+        $options['license_key'] = $this->licenseKey;
+        $url = 'https://minfraud1.maxmind.com/app/ccv2r?' . http_build_query($options);
+        $minfraud = array();
+        $minfraud_contents = YdUrlReader::getUrl($url);
+        $minfraud_array = explode(';', $minfraud_contents);
+        foreach ($minfraud_array as $minfraud_row) {
+            list($key, $val) = explode('=', $minfraud_row);
+            $minfraud[$key] = $val;
+        }
+        return $minfraud;
+    }
+
+}
