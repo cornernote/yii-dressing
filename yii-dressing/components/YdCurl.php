55acb412522b03ecc6ebf62439a3b2ec4ea8885f
cornernote <cornernote@gmail.com>
2014-04-23 10:58:31 +0930

    add missing properties

--- yii-dressing/components/YdCurl.php
+++ yii-dressing/components/YdCurl.php
@@ -9,17 +9,25 @@ class YdCurl
 {
 
     /**
+     * @var string
+     */
+    public static $agent;
+
+    /**
+     * @var string
+     */
+    public static $referer;
+
+    /**
      * @param string $url
      * @param string|array $post
-     * @param null|string $referer
      * @return bool|string
      */
-    static public function download($url, $post = array(), $referer = null)
+    static public function download($url, $post = array())
     {
         $ch = curl_init();
         //curl_setopt($ch, CURLOPT_VERBOSE, true);
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_USERAGENT, self::$agent);
         curl_setopt($ch, CURLOPT_AUTOREFERER, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
@@ -27,8 +35,11 @@ class YdCurl
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, is_string($post) ? $post : http_build_query($post));
         }
-        if ($referer) {
-            curl_setopt($ch, CURLOPT_REFERER, $referer);
+        if (self::$referer) {
+            curl_setopt($ch, CURLOPT_REFERER, self::$referer);
+        }
+        if (self::$agent) {
+            curl_setopt($ch, CURLOPT_USERAGENT, self::$agent);
         }
         $_url = parse_url($url);
         $cookieFile = Yii::app()->getRuntimePath() . '/cookies/' . md5($_url['host']);
