dbb032c8ecedacb8c0f99a04123c406f48e8c8e3
cornernote <cornernote@gmail.com>
2014-05-12 13:59:20 +0930

    tune bpay logo

--- yii-dressing/views/misc/bpay.php
+++ yii-dressing/views/misc/bpay.php
@@ -7,7 +7,7 @@
 ?>
 <table border="0" cellpadding="0" cellspacing="0" style="width: 200px;font-family: Arial, Helvetica, sans-serif;background-color: #FFFFFF;margin: 0px; padding:7px; border:1px solid #000;">
     <tr>
-        <td width="1%" style="width: 42px;"><img src="<?php echo $logo; ?>"/></td>
+        <td width="1%" style="width: 42px;vertical-align:middle;"><img src="<?php echo $logo; ?>"/></td>
         <td style="border: 1px solid #000000;padding: 0px 8px;height: 54px;width: 158px;">
             <p style="font-size: 13px;text-transform: capitalize;color: #000000;white-space: nowrap;line-height: 22px;font-weight: normal;">
                 Biller Code: <?php echo $bPay->billerCode; ?>
