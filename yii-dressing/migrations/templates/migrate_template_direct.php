4a2088c13fa80ee233210c2ae33b0292155063d0
zainengineer <zainengineer@gmail.com>
2014-05-27 06:14:40 +0930

    An optional direct migration template

---
+++ yii-dressing/migrations/templates/migrate_template_direct.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * {ClassName}
+ */
+class {ClassName} extends YdDbMigration
+{
+
+    public function safeUp()
+    {
+
+        $this->qs("
+
+        ");
+
+        /**
+        // Run single query:
+        $this->q("SINGLE SQL");
+
+        // Run multiple queries:
+        $this->qs("
+            FIRST SQL;
+            ANOTHER SQL;
+        ");
+
+        // Import sql file:
+        $this->import('{ClassName}.sql');
+
+         **/
+
+    }
+
+}
