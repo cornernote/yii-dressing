7ff51b5ef8d9f50d307a650770f731cbc64fbb7a
cornernote <cornernote@gmail.com>
2014-06-08 13:22:06 +0930

    update bootstrap-modal with improved version

--- yii-dressing/widgets/YdModal.php
+++ yii-dressing/widgets/YdModal.php
@@ -13,77 +13,45 @@
  */
 class YdModal extends CWidget
 {
-    // function to init the widget
     /**
      *
      */
     public function init()
     {
-        $this->publishAssets();
-    }
-
-    // function to publish and register assets on page
-    /**
-     *
-     */
-    public function publishAssets()
-    {
-        $baseUrl = Yii::app()->assetManager->publish(Yii::getPathOfAlias('dressing.assets.modal-responsive-fix'), false, 1, YII_DEBUG);
-
-        $cs = Yii::app()->clientScript;
-
-        $this->registerScript();
-
         // fix modals on mobile devices
-        // http://niftylettuce.github.com/twitter-bootstrap-jquery-plugins/
-        $cs->registerScriptFile($baseUrl . '/touchscroll.js', CClientScript::POS_HEAD, array('order' => 1));
-        $cs->registerScriptFile($baseUrl . '/modal-responsive-fix.min.js', CClientScript::POS_HEAD, array('order' => 1));
-        $cs->registerCSS('modal-responsive-fix', '.modal-body { -webkit-overflow-scrolling:touch; } body.modal-open{overflow: hidden;} @media (max-width: 767px) {.modal.fade.in {top: 10px !important;}}', '', array('order' => 10));
+        $baseUrl = Yii::app()->assetManager->publish(Yii::getPathOfAlias('dressing.assets.bootstrap-modal'), false, 1, YII_DEBUG);
+        $cs = Yii::app()->clientScript;
+        $cs->registerCssFile($baseUrl . '/css/bootstrap-modal.css');
+        $cs->registerScriptFile($baseUrl . '/js/bootstrap-modalmanager.js', CClientScript::POS_END);
+        $cs->registerScriptFile($baseUrl . '/js/bootstrap-modal.js', CClientScript::POS_END);
 
-        // load here so ajax requests dont need it
+        // load yiiactiveform so it's not needed to load in remote modal
         Yii::app()->getClientScript()->registerCoreScript('yiiactiveform');
-    }
-
-    /**
-     *
-     */
-    protected function registerScript()
-    {
-        $this->beginWidget('dressing.widgets.YdJavaScriptWidget', array('position' => CClientScript::POS_END));
-        ?>
-        <script type="text/javascript">
-            $('.modal').modalResponsiveFix();
-            $('.modal').touchScroll();
-        </script><?php
-        $this->endWidget();
 
         // Support for AJAX loaded modal window.
         $this->beginWidget('dressing.widgets.YdJavaScriptWidget', array('position' => CClientScript::POS_END));
         ?>
         <script type="text/javascript">
-            $('[data-toggle="modal-remote"]').click(function (e) {
+            $(document).on('click', '[data-toggle="modal-remote"]', function (e) {
                 e.preventDefault();
-                var url = $(this).attr('href');
-                var modalRemote = $('#modal-remote');
+                var $modalRemote = $('#modal-remote'),
+                    url = $(this).attr('href');
 
                 $.ajax({
                     url: url,
                     beforeSend: function (data) {
-                        if (!modalRemote.length) modalRemote = $('<div class="modal hide fade" id="modal-remote"></div>');
-                        modalRemote.html('<div class="modal-header"><h3><?php echo Yii::t('dressing', 'Loading...'); ?></h3></div><div class="modal-body"><div class="modal-remote-indicator"></div>');
-                        modalRemote.modalResponsiveFix();
-                        modalRemote.touchScroll();
-                        modalRemote.modal();
+                        if (!$modalRemote.length) $modalRemote = $('<div class="modal hide fade" id="modal-remote" data-width="940"></div>');
+                        $modalRemote.html('<div class="modal-header"><h3><?php echo Yii::t('dressing', 'Loading...'); ?></h3></div><div class="modal-body"><div class="modal-remote-indicator"></div>');
+                        $modalRemote.modal();
                     },
                     success: function (data) {
-                        modalRemote.html(data);
+                        $modalRemote.html(data);
                         $(window).resize();
-                        //modalRemote.children('input:text:visible:first').focus();
-                        $('#modal-remote input:text:visible:first').focus();
+                        $modalRemote.find('input:text:visible:first').focus();
                     },
                     error: function (XMLHttpRequest, textStatus, errorThrown) {
-                        modalRemote.children('.modal-header').html('<button type="button" class="close" data-dismiss="modal"><i class="icon-remove"></i></button><h3><?php echo Yii::t('dressing', 'Error!'); ?></h3>');
-                        modalRemote.children('.modal-body').html(XMLHttpRequest.responseText);
+                        $modalRemote.children('.modal-header').html('<button type="button" class="close" data-dismiss="modal"><i class="icon-remove"></i></button><h3><?php echo Yii::t('dressing', 'Error!'); ?></h3>');
+                        $modalRemote.children('.modal-body').html(XMLHttpRequest.responseText);
                     }
                 });
             });
